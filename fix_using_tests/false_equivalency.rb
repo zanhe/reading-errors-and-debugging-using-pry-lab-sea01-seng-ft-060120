85a7074212a1507a81e94da430dec305777041aa
Zana H <zhertica@gmail.com>
2020-05-16 06:24:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,10 +12,10 @@ def prompt_user
 end
 
 def selection(num)
-   binding.pry
-  if num = 1
+  
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
