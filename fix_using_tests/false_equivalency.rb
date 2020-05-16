6ac59a31a3202269e09a1f3312b0553cd317080a
Zana H <zhertica@gmail.com>
2020-05-16 06:37:07 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,4 +1,4 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def get_user_input
   gets.chomp
@@ -6,14 +6,16 @@ end
 
 def prompt_user
   puts "What would you like to do?"
+ 
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
 end
 
 def selection(num)
-  if num = 1
+  
+  if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
