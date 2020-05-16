e789a7c2e228b1721357c5f2bc8c0ebb4f2bc5f7
Zana H <zhertica@gmail.com>
2020-05-16 06:27:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,7 +4,6 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
-  binding.pry
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
 end
